ea2e7d488a64a555cd37af2e9fb7d5ed38423a08
hrishikesh bhagwat <hrishikesh.bhagwat@pwc.com>
2021-02-07 09:56:34 -0800

    ignore environment files which have sensitive config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-// your firebase config here
+  firebaseConfig:{
+    apiKey: "AIzaSyC0l10-9VSLpFBMokFDDxNQkGpwBDNfHes",
+    authDomain: "bestdealsoc-140e8.firebaseapp.com",
+    projectId: "bestdealsoc-140e8",
+    storageBucket: "bestdealsoc-140e8.appspot.com",
+    messagingSenderId: "249410480258",
+    appId: "1:249410480258:web:621683a4fc64f116a7ac9b",
+    measurementId: "G-ZZXD491Y2W"
+  }
 };
 
 /*
